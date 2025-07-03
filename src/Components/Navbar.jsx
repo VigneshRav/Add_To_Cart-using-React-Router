57bc16031b327e9ddf948945ed0d3e4fb3ab7df7
VigneshRav <vignesh212000@gmail.com>
2025-07-03 22:34:01 +0530

    Styles updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ import { IoCartOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ cartCount }) => (
-  <nav className="text-white p-5 bg-gradient-to-l from-purple-900 via-blue-800 to-violet-950">
+  <nav className="text-white p-5 bg-gradient-to-l from-cyan-900 via-emerald-400 to-lime-900">
     <div className="container mx-auto flex justify-between">
       <Link to="/" className="text-4xl italic font-serif font-extralight">
         ShopMart!
       </Link>
-      <button className="bg-gray-800 px-2 py-2 rounded focus:outline-none hover:bg-red-700 cursor-pointer">
+      <button className="bg-orange-800 px-2 py-2 rounded-lg focus:outline-none hover:bg-red-700 cursor-pointer">
         <Link to="/cart">
           <div className="flex justify-between gap-2">
             <p className="pt-1">
